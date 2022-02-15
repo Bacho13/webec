70d52957f073f723d1112ad0341865695b1e9fd4
Bacho13 <bacholashkhia13@gmail.com>
2022-02-15 14:35:12 +0400

    add init files/folders

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+
+const Container = styled.div`
+    
+`
+
+function CategoryItem({item}) {
+  return (
+    <Container>
+         HEllo
+    </Container>
+  )
+}
+
+export default CategoryItem
